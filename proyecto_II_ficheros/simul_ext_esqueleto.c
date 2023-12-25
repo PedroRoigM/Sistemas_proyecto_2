1430889eed73455cc3e1a262c81eece7b0145a0a
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2023-12-25 10:18:40 +0100

    Acabada la función de ComprobarComando
    Ya se perciben los argumentos que siguen a la orden

--- proyecto_II_ficheros/simul_ext_esqueleto.c
+++ proyecto_II_ficheros/simul_ext_esqueleto.c
@@ -41,7 +41,7 @@ int main()
     int entradadir;
     int grabardatos;
     FILE *fent;
-	
+
     // Lectura del fichero completo de una sola vez
     //...
 
@@ -62,11 +62,9 @@ int main()
             printf(">> ");
             fflush(stdin);
             fgets(comando, LONGITUD_COMANDO, stdin);
-			
 
         } while (ComprobarComando(comando, orden, argumento1, argumento2) != 0);
-		
-		
+
         if (strcmp(orden, "dir") == 0)
         {
             Directorio(directorio, &ext_blq_inodos);
@@ -75,43 +73,47 @@ int main()
         else if (strcmp(orden, "info\0") == 0)
         {
             LeeSuperBloque(&ext_superblock);
-            //continue;
+            // continue;
         }
         else if (strcmp(orden, "bytemaps\0") == 0)
         {
-			printf("Bytemaps:\n");
+            printf("Bytemaps:\n");
             Printbytemaps(&ext_bytemaps);
             continue;
         }
         else if (strcmp(orden, "imprimir\0") == 0)
         {
-            if (!Imprimir(directorio, &ext_blq_inodos, memdatos, argumento1)) {
-                    printf("Error al imprimir el fichero\n");
-                }
+            if (!Imprimir(directorio, &ext_blq_inodos, memdatos, argumento1))
+            {
+                printf("Error al imprimir el fichero\n");
+            }
             continue;
         }
         else if (strcmp(orden, "rename\0") == 0)
         {
-            if (!Renombrar(directorio, &ext_blq_inodos, argumento1, argumento2)) {
-                    printf("Error al renombrar el fichero\n");
-                }
+            if (!Renombrar(directorio, &ext_blq_inodos, argumento1, argumento2))
+            {
+                printf("Error al renombrar el fichero\n");
+            }
             continue;
         }
         else if (strcmp(orden, "remove\0") == 0)
         {
-            if (!Borrar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, argumento1, fent)) {
-                    printf("Error al eliminar el fichero\n");
-                }
+            if (!Borrar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, argumento1, fent))
+            {
+                printf("Error al eliminar el fichero\n");
+            }
             continue;
         }
         else if (strcmp(orden, "copy\0") == 0)
         {
-            if (!Copiar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, memdatos, argumento1, argumento2, fent)) {
-                    printf("Error al copiar el fichero\n");
-                }
+            if (!Copiar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, memdatos, argumento1, argumento2, fent))
+            {
+                printf("Error al copiar el fichero\n");
+            }
             continue;
         }
-        
+
         //...
         // Escritura de metadatos en comandos rename, remove, copy
         /*Grabarinodosydirectorio(directorio, &ext_blq_inodos, fent);
@@ -128,43 +130,75 @@ int main()
             fclose(fent);
             return 0;
         }
-        
     }
 }
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps)
 {
-	printf("Inodos: ");
-	for(int i = 0; i < MAX_INODOS; i++){
-		printf("%d ", ext_bytemaps->bmap_inodos[i]);
-	}
-	printf("\nBloques [0 - 25]: ");
-	for(int i = 0; i < 25; i++){
-		printf("%d ", ext_bytemaps->bmap_bloques[i]);
-	}
-	printf("\n");
+    printf("Inodos: ");
+    for (int i = 0; i < MAX_INODOS; i++)
+    {
+        printf("%d ", ext_bytemaps->bmap_inodos[i]);
+    }
+    printf("\nBloques [0 - 25]: ");
+    for (int i = 0; i < 25; i++)
+    {
+        printf("%d ", ext_bytemaps->bmap_bloques[i]);
+    }
+    printf("\n");
 }
 int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2)
 {
-    const char *comandosPosibles[] = {"info", "bytemaps", "dir", "rename","imprimir", "remove", "copy", "salir"};
+    const char *comandosPosibles[] = {"info", "bytemaps", "dir", "rename", "imprimir", "remove", "copy", "salir"};
     int i = 0;
+    int contadorArgumanto = 0;
     int valorRetorno = 1;
-    while (strcomando[i] != ' ' && strcomando[i] != '\0') {
+    while (strcomando[i] != ' ' && strcomando[i] != '\0')
+    {
         orden[i] = strcomando[i];
         i++;
     }
-    orden[i-1] = '\0'; 
-	
-    for (int j = 0; j < 8 && valorRetorno == 1; j++) {
-		
-        if (strcmp(comandosPosibles[j], orden) == 0) {
+    if (strcomando[i] == ' ')
+    {
+        orden[i] = '\0';
+        i++;
+        while (strcomando[i] != ' ' && strcomando[i] != '\0')
+        {
+            argumento1[contadorArgumanto] = strcomando[i];
+            i++;
+            contadorArgumanto++;
+        }
+        argumento1[contadorArgumanto] = '\0';
+        if (strcomando[i] == ' ')
+        {
+            i++;
+            contadorArgumanto = 0;
+            while (strcomando[i] != ' ' && strcomando[i] != '\0')
+            {
+
+                argumento2[contadorArgumanto] = strcomando[i];
+                i++;
+                contadorArgumanto++;
+            }
+            argumento2[contadorArgumanto] = '\0';
+        }
+    }
+    else
+        orden[i - 1] = '\0';
+
+    for (int j = 0; j < 8 && valorRetorno == 1; j++)
+    {
+
+        if (strcmp(comandosPosibles[j], orden) == 0)
+        {
             valorRetorno = 0;
         }
     }
 
-    if (valorRetorno == 1) {
+    if (valorRetorno == 1)
+    {
         printf("ERROR: Comando ilegal [bytemaps, copy, dir, info, imprimir, rename, remove, salir]\n");
     }
-	
+
     return valorRetorno;
 }
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup)
@@ -176,46 +210,55 @@ void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup)
     printf("Inodos libres: %u\n", psup->s_free_inodes_count);
     printf("Primer bloque de datos: %u\n", psup->s_first_data_block);
     printf("Tamaño del bloque: %u bytes\n", psup->s_block_size);
-
 }
 int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombre)
 {
 }
 void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos)
 {
-	for (int i = 0; i < MAX_INODOS; i++) {
-		if (directorio[i].dir_nfich[0] != '\0') {
-		  // Ignorar la entrada especial del directorio raíz
-		  if (strcmp(directorio[i].dir_nfich, ".") != 0 && strcmp(directorio[i].dir_nfich, "..") != 0) {
-			// Verificar existencia del archivo
-			if (inodos->blq_inodos[directorio[i].dir_inodo].size_fichero > 0) {
-			  // Imprimir información del fichero
-			  printf("Nombre: %s\n", directorio[i].dir_nfich);
-			  printf("Tamaño: %u\n", inodos->blq_inodos[directorio[i].dir_inodo].size_fichero);
-			  printf("Inodo: %u\n", directorio[i].dir_inodo);
+    for (int i = 0; i < MAX_INODOS; i++)
+    {
+        if (directorio[i].dir_nfich[0] != '\0')
+        {
+            // Ignorar la entrada especial del directorio raíz
+            if (strcmp(directorio[i].dir_nfich, ".") != 0 && strcmp(directorio[i].dir_nfich, "..") != 0)
+            {
+                // Verificar existencia del archivo
+                if (inodos->blq_inodos[directorio[i].dir_inodo].size_fichero > 0)
+                {
+                    // Imprimir información del fichero
+                    printf("Nombre: %s\n", directorio[i].dir_nfich);
+                    printf("Tamaño: %u\n", inodos->blq_inodos[directorio[i].dir_inodo].size_fichero);
+                    printf("Inodo: %u\n", directorio[i].dir_inodo);
 
-			  // Imprimir bloques que ocupa
-			  printf("Bloques:");
-			  for (int j = 0; j < MAX_NUMS_BLOQUE_INODO; j++) {
-				if (inodos->blq_inodos[directorio[i].dir_inodo].i_nbloque[j] != 0) {
-				  printf(" %u", inodos->blq_inodos[directorio[i].dir_inodo].i_nbloque[j]);
-				}
-			  }
-			  printf("\n");
+                    // Imprimir bloques que ocupa
+                    printf("Bloques:");
+                    for (int j = 0; j < MAX_NUMS_BLOQUE_INODO; j++)
+                    {
+                        if (inodos->blq_inodos[directorio[i].dir_inodo].i_nbloque[j] != 0)
+                        {
+                            printf(" %u", inodos->blq_inodos[directorio[i].dir_inodo].i_nbloque[j]);
+                        }
+                    }
+                    printf("\n");
 
-			  printf("---------------\n");
-			} else {
-			  printf("El archivo %s no existe.\n", directorio[i].dir_nfich);
-			}
-		  }
-		}
-	  }
+                    printf("---------------\n");
+                }
+                else
+                {
+                    printf("El archivo %s no existe.\n", directorio[i].dir_nfich);
+                }
+            }
+        }
+    }
 }
 int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombreantiguo, char *nombrenuevo)
 {
 }
 int Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_DATOS *memdatos, char *nombre)
 {
+    printf("Nombre %s\n", nombre);
+    return 0;
 }
 int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
            EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
@@ -224,8 +267,6 @@ int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
 }
 int Copiar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock, EXT_DATOS *memdatos, char *nombreorigen, char *nombredestino, FILE *fich)
 {
-	
-		
 }
 void Grabarinodosydirectorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, FILE *fich)
 {

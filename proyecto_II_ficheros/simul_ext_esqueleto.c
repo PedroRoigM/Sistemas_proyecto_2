6abc29689b00cd99a30f4184b6510f590b5bdfed
iviguio <i.guio.aunon@gmail.com>
2023-12-28 10:22:33 +0100

    funciones grabar creadas
    Se ha creado las funciones grabarinodos, grabarbytemaps. grabarbloque, grabardatos. queda implementarlas en el codigo para modificar el binario

--- proyecto_II_ficheros/simul_ext_esqueleto.c
+++ proyecto_II_ficheros/simul_ext_esqueleto.c
@@ -397,13 +397,27 @@ int Copiar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_BYTE_MAPS *e
 }
 void Grabarinodosydirectorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, FILE *fich)
 {
+    fseek(fich, 3 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(inodos, SIZE_BLOQUE, 1, fich);
+    fseek(fich, 4 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(directorio, SIZE_BLOQUE, 1, fich);
+    fflush(fich);
 }
 void GrabarByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *fich)
 {
+    fseek(fich, 1 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(ext_bytemaps, SIZE_BLOQUE, 1, fich);
+    fflush(fich);
 }
 void GrabarSuperBloque(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *fich)
 {
+    fseek(fich, 0 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(ext_superblock, SIZE_BLOQUE, 1, fich);
+    fflush(fich);
 }
 void GrabarDatos(EXT_DATOS *memdatos, FILE *fich)
 {
+    fseek(fich, 4 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(memdatos, SIZE_BLOQUE, MAX_BLOQUES_DATOS, fich);
+    fflush(fich);
 }

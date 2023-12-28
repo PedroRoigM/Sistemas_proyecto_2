5a8f3d9d972012daf64c69b317273a5c2bbb763c
iviguio <i.guio.aunon@gmail.com>
2023-12-28 11:02:26 +0100

    error grabar solucionado
    solucionado el error que daba al guardar en el binario. todo funciona correctamente

--- proyecto_II_ficheros/simul_ext_esqueleto.c
+++ proyecto_II_ficheros/simul_ext_esqueleto.c
@@ -405,22 +405,33 @@ int Copiar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_BYTE_MAPS *e
 }
 void Grabarinodosydirectorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, FILE *fich)
 {
-    fseek(fich, 3 * SIZE_BLOQUE, SEEK_SET);
+    /*fseek(fich, 3 * SIZE_BLOQUE, SEEK_SET);
     fwrite(inodos, SIZE_BLOQUE, 1, fich);
     fseek(fich, 4 * SIZE_BLOQUE, SEEK_SET);
     fwrite(directorio, SIZE_BLOQUE, 1, fich);
+    fflush(fich);*/
+    fseek(fich, 2 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(inodos, sizeof(EXT_BLQ_INODOS), 1, fich);
+    fseek(fich, 3 * SIZE_BLOQUE, SEEK_SET);
+    fwrite(directorio, sizeof(EXT_ENTRADA_DIR) * MAX_FICHEROS, 1, fich);
     fflush(fich);
 }
 void GrabarByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *fich)
 {
-    fseek(fich, 1 * SIZE_BLOQUE, SEEK_SET);
+    /*fseek(fich, 1 * SIZE_BLOQUE, SEEK_SET);
     fwrite(ext_bytemaps, SIZE_BLOQUE, 1, fich);
+    fflush(fich);*/
+    fseek(fich, SIZE_BLOQUE, SEEK_SET);
+    fwrite(ext_bytemaps, sizeof(EXT_BYTE_MAPS), 1, fich);
     fflush(fich);
 }
 void GrabarSuperBloque(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *fich)
 {
-    fseek(fich, 0 * SIZE_BLOQUE, SEEK_SET);
+    /*fseek(fich, 0 * SIZE_BLOQUE, SEEK_SET);
     fwrite(ext_superblock, SIZE_BLOQUE, 1, fich);
+    fflush(fich);*/
+    fseek(fich, 0, SEEK_SET);
+    fwrite(ext_superblock, sizeof(EXT_SIMPLE_SUPERBLOCK), 1, fich);
     fflush(fich);
 }
 void GrabarDatos(EXT_DATOS *memdatos, FILE *fich)
